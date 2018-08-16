91c7b051c6277d6d1fddc3ff3984c7d89a9fa937
JOsue <josuegbl@gmail.com>
2018-08-16 09:28:09 -0400

    added test autoclassify

---
+++ cryomethods/tests/__init__.py
@@ -0,0 +1,3 @@
+
+from .test_volume_selector import *
+from .test_autoclassify import *
---
+++ cryomethods/tests/test_autoclassify.py
@@ -0,0 +1,118 @@
+# **************************************************************************
+# *
+# * Authors:     Josue Gomez Blanco (josue.gomez-blanco@mcgill.ca)
+# *              Javier Vargas Balbuena (javier.vargasbalbuena@mcgill.ca)
+# *
+# * Department of Anatomy and Cell Biology, McGill University
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+from pyworkflow.tests import *
+from pyworkflow.em import ProtImportParticles, ProtImportVolumes
+from cryomethods.protocols import ProtAutoClassifier
+
+
+class TestBase(BaseTest):
+    @classmethod
+    def setData(cls, dataProject='relion_tutorial'):
+        cls.dataset = DataSet.getDataSet(dataProject)
+        cls.particlesFn = cls.dataset.getFile('import/case2/particles.sqlite')
+        cls.volumes = cls.dataset.getFile('import/case2/')
+
+    def checkOutput(self, prot, outputName, conditions=[]):
+        """ Check that an output was generated and
+        the condition is valid.
+        """
+        o = getattr(prot, outputName, None)
+        locals()[outputName] = o
+        self.assertIsNotNone(o, "Output: %s is None" % outputName)
+        for cond in conditions:
+            self.assertTrue(eval(cond), 'Condition failed: ' + cond)
+
+    @classmethod
+    def runImportParticles(cls, pattern, samplingRate, checkStack=False):
+        """ Run an Import particles protocol. """
+        protImport = cls.newProtocol(ProtImportParticles,
+                                     importFrom=4,
+                                     sqliteFile=pattern,
+                                     samplingRate=samplingRate,
+                                     checkStack=checkStack)
+        cls.launchProtocol(protImport)
+        # check that input images have been imported (a better way to do this?)
+        if protImport.outputParticles is None:
+            raise Exception('Import of images: %s, failed. outputParticles '
+                            'is None.' % pattern)
+        return protImport
+
+    @classmethod
+    def runImportVolumes(cls, pattern, samplingRate):
+        """ Run an Import particles protocol. """
+        protImport = cls.newProtocol(ProtImportVolumes,
+                                     filesPath=pattern,
+                                     filesPattern='relion*_class*.mrc',
+                                     samplingRate=samplingRate)
+        cls.launchProtocol(protImport)
+        return protImport
+
+    @classmethod
+    def runImportSingleVolume(cls, pattern, samplingRate):
+        """ Run an Import particles protocol. """
+        protImport = cls.newProtocol(ProtImportVolumes,
+                                     filesPath=pattern,
+                                     filesPattern='relion*_class001.mrc',
+                                     samplingRate=samplingRate)
+        cls.launchProtocol(protImport)
+        return protImport
+
+
+class TestAutoClasifier(TestBase):
+    @classmethod
+    def setUpClass(cls):
+        setupTestProject(cls)
+        TestBase.setData()
+        cls.protImport = cls.runImportParticles(cls.particlesFn, 7.08)
+        cls.protImportVol = cls.runImportSingleVolume(cls.volumes, 7.08)
+
+    def testInitialVolumeSelector(self):
+        def _runAutoClassifier(doGpu=False, label=''):
+            print label
+            autoClassifierProt = self.newProtocol(ProtAutoClassifier,
+                                                  numberOfIterations=10,
+                                                  level=2,
+                                                  numberOfMpi=4,
+                                                  numberOfThreads=1)
+            autoClassifierProt.setObjLabel(label)
+            autoClassifierProt.inputParticles.set(
+                self.protImport.outputParticles)
+            autoClassifierProt.inputVolumes.set(self.protImportVol.outputVolume)
+
+            autoClassifierProt.doGpu.set(doGpu)
+
+            self.launchProtocol(autoClassifierProt)
+            return autoClassifierProt
+
+        def _checkAsserts(relionProt):
+            self.assertIsNotNone(relionProt.outputVolumes, "There was a "
+                                                           "problem with "
+                                                           "Initial Volume "
+                                                           "Selector")
+
+        volSelGpu = _runAutoClassifier(True, "Run Auto-classifier GPU")
+        _checkAsserts(volSelGpu)
