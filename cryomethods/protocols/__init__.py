be9088e11e417fcc2dc30608eba973eaf3ecc0e7
Alex <alex.alejandro123@hotmail.com>
2019-07-10 15:46:00 -0400

    Added protocol for screening using deep learning

--- cryomethods/protocols/__init__.py
+++ cryomethods/protocols/__init__.py
@@ -29,3 +29,4 @@ from protocol_2d_auto_classifier import Prot2DAutoClassifier
 from protocol_auto_classifier import Prot3DAutoClassifier
 from protocol_volume_selector import ProtInitialVolumeSelector
 
+from protocol_CNN import ProtSCNN
