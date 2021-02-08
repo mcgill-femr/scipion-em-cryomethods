9dab4d06664578600c8c2750365e47548a7ea1fb
josuegbl <josuegbl@gmail.com>
2021-02-08 12:52:03 +0100

    updated code to Python 3.8

--- cryomethods/__init__.py
+++ cryomethods/__init__.py
@@ -31,7 +31,7 @@ import os, sys
 import pwem
 import pyworkflow.utils as pwutils
 
-from .constants import (RELION_CRYOMETHODS_HOME, CRYOMETHODS_HOME, V3_1,
+from .constants import (RELION_CRYOMETHODS_HOME, CRYOMETHODS_HOME, V3_1, V3_0,
                         XMIPP_CRYOMETHODS_HOME, NMA_HOME)
 
 # from bibtex import _bibtex # Load bibtex dict with references
@@ -171,20 +171,20 @@ class Plugin(pwem.Plugin):
     @classmethod
     def getSupportedRelionVersions(cls):
         """ Return the list of supported binary versions. """
-        return [V3_1]
+        return [V3_0, V3_1]
 
     @classmethod
     def IS_RELION_30(cls):
-        return cls.getActiveVersion().startswith('3.0')
+        return cls.getActiveRelionVersion().startswith('3.0')
 
     @classmethod
     def IS_RELION_31(cls):
         # avoid using this, IS_GT30 is preferred
-        return cls.getActiveVersion().startswith('3.1')
+        return cls.getActiveRelionVersion().startswith('3.1')
 
     @classmethod
     def IS_RELION_GT30(cls):
-        return not cls.getActiveVersion().startswith('3.0')
+        return not cls.getActiveRelionVersion().startswith('3.0')
 
     @classmethod
     def getSupportedVersions(cls):
